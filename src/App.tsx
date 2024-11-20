a472c561f31f3c4bfd35652033a57423dc048372
Bernard Namangala <58230307+Bernard-Namangala@users.noreply.github.com>
2024-11-20 19:05:10 +0200

    implement mint new position

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,19 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import PoolPage from "./pages/Pool";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Router>
+        <Routes>
+          <Route path="/pool/" element={<PoolPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/types/index.ts
@@ -0,0 +1,25 @@
+export type TokenData = {
+  symbol: string;
+  name: string;
+  decimals: number;
+};
+
+export type Action = "mint" | "increase" | "remove" | null;
+export type ModalProps = {
+  positionId: number | null;
+  onClose: () => void;
+};
+export type PoolData = {
+  token0: string;
+  token1: string;
+  tick: string;
+  sqrtPriceX96: string;
+  fee: number;
+  liquidity: number;
+};
+
+// Define the type for exchange rates
+export type ExchangeRates = {
+  token1: number;
+  token0: number;
+};
